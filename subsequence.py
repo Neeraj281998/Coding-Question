49579b36ba9274334d7ff1712c8cbdcad68a0ba7
Neeraj281998 <82948400+Neeraj281998@users.noreply.github.com>
2022-01-22 11:44:03 +0530

    Update subsequence.py

--- subsequence.py
+++ subsequence.py
@@ -1,3 +1,4 @@
+#Print subsequences
 def subseq(s,main):
     if main=="":
         print(s)
@@ -7,3 +8,20 @@ def subseq(s,main):
     subseq(s,main[1:]) 
 
 subseq("","abc")
+
+#Print subsequences in Array 
+
+
+def subseq(s,main):
+    if main=="":
+        list=[]
+        list.append(s)
+        return list
+    ch=main[0]
+    left=subseq(s+ch,main[1:])
+    right=subseq(s,main[1:]) 
+
+    left+=right
+    return left
+
+print(subseq("","abc"))
